fb3a656e7d8f7d7fc68166611d07d45739b54e6f
Emmanuelle <emmanuelle.em.menard@gmail.com>
2021-07-23 15:55:29 +0200

    Update UI

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,9 +1,10 @@
 import 'dart:developer';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:loan_simulator/view_model/homeViewModel.dart';
 import 'package:provider/provider.dart';
-
 
 TextEditingController capitalController = TextEditingController();
 TextEditingController lengthController = TextEditingController();
@@ -11,9 +12,6 @@ TextEditingController rateController = TextEditingController();
 int capital;
 int length;
 int rate;
-
-
-
 
 class Home extends StatefulWidget {
   const Home({Key key}) : super(key: key);
@@ -49,51 +47,75 @@ class _HomeState extends State<Home> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Text("Calculer vos mensualités"),
-            TextFormField(
-              decoration: InputDecoration(hintText: "Capital emprunté"),
-              controller: capitalController,
-              keyboardType: TextInputType.number,
-              validator: (text) {
-                if (text == null || text.isEmpty) {
-                  return "error";
-                }
-                return null;
-              },
+            Text("Calculer vos mensualités", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35, color: Colors.indigo), textAlign: TextAlign.center,),
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 10, right: 10, top: 5,
+              ),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    hintText: "Capital emprunté",
+                    border: OutlineInputBorder(),
+                    icon: Icon(Icons.house)),
+                controller: capitalController,
+                keyboardType: TextInputType.number,
+                validator: (text) {
+                  if (text == null || text.isEmpty) {
+                    return "error";
+                  }
+                  return null;
+                },
+              ),
             ),
             Row(
               mainAxisSize: MainAxisSize.min,
               children: [
                 Expanded(
-                  child: TextFormField(
-                    decoration: InputDecoration(hintText: "Durée"),
-                    controller: lengthController,
-                    keyboardType: TextInputType.number,
-                    validator: (text) {
-                      if (text == null || text.isEmpty) {
-                        return "error";
-                      }
-                      return null;
-                    },
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      left: 10, right: 10, top: 5,
+                    ),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                          hintText: "Durée",
+                          border: OutlineInputBorder(),
+                          icon: Icon(Icons.calendar_today_outlined)),
+                      controller: lengthController,
+                      keyboardType: TextInputType.number,
+                      validator: (text) {
+                        if (text == null || text.isEmpty) {
+                          return "error";
+                        }
+                        return null;
+                      },
+                    ),
                   ),
                 ),
                 Expanded(
-                  child: TextFormField(
-                    decoration: InputDecoration(hintText: "Taux"),
-                    controller: rateController,
-                    keyboardType: TextInputType.number,
-                    validator: (text) {
-                      if (text == null || text.isEmpty) {
-                        return "error";
-                      }
-                      return null;
-                    },
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      left: 10, right: 10, top: 5,
+                    ),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                          hintText: "Taux",
+                          border: OutlineInputBorder(),
+                          icon: Icon(Icons.attach_money)),
+                      controller: rateController,
+                      keyboardType: TextInputType.number,
+                      validator: (text) {
+                        if (text == null || text.isEmpty) {
+                          return "error";
+                        }
+                        return null;
+                      },
+                    ),
                   ),
                 ),
               ],
             ),
             GestureDetector(
-              child: TextButton(
+              child: ElevatedButton(
                   onPressed: () async {
                     if (formKey.currentState.validate()) {
                       capital = int.parse(capitalController.text);
@@ -102,7 +124,7 @@ class _HomeState extends State<Home> {
                       showDialog(context: context, builder: showAlertDialog);
                     }
                   },
-                  child: Text("Calculer")),
+                  child: Text("Calculer", style: TextStyle(fontSize: 20),)),
             )
           ],
         ),
@@ -115,9 +137,13 @@ Widget showAlertDialog(BuildContext context) {
   Provider.of<HomeViewModel>(context).getResult(capital, length, rate);
   var result = Provider.of<HomeViewModel>(context).result;
   return AlertDialog(
-    content: Text("Vos mensualités sont de $result"),
-    actions: [TextButton(onPressed: () {
-      Navigator.of(context).pop();
-    }, child: Text("ok"))],
+    content: Text("Vos mensualités sont de $result €"),
+    actions: [
+      TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text("ok"))
+    ],
   );
 }
