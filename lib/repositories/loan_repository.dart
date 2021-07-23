7e290ed2538fbf4cc9dfc7799437252beb00f014
Emmanuelle <emmanuelle.em.menard@gmail.com>
2021-07-23 15:27:04 +0200

    Update function for loan calculator

--- lib/repositories/loan_repository.dart
+++ lib/repositories/loan_repository.dart
@@ -8,14 +8,14 @@ class LoanRepository {
     int result;
 
     // calculate capital*(rate/12)
-    var rateInt = (rate ~/ 100);
+    var rateInt = (Decimal.fromInt(rate)/Decimal.fromInt(100)).toDouble();
     var durationInMonth = (length*12);
-    var firstValue = Decimal.fromInt(capital*(rateInt~/durationInMonth));
+    var firstValue = capital*(rateInt/12);
     // calculate 1-(1+(rate/12))^durationInMonth
-    var ratePerMonthPlusOne = 1+(rateInt~/12);
-    var exponent = pow(ratePerMonthPlusOne, durationInMonth);
-    var secondValue = Decimal.fromInt(1-exponent);
-    result = (firstValue/secondValue).toInt();
+    var ratePerMonthPlusOne = 1+(rateInt/12);
+    var exponent = pow(ratePerMonthPlusOne, -durationInMonth);
+    var secondValue = 1-exponent;
+    result = (firstValue/secondValue).truncate();
 
     return result;
   }
